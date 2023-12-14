4612b0f0ed56210acf56b6ed3df17ba8a21b0e2a
vinc-rot <vrhack1@gmail.com>
2023-12-14 14:35:36 +0100

    Update on header

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 import { Footer } from '../../../payload/payload-types'
-import { fetchFooter, fetchGlobals } from '../../_api/fetchGlobals'
-import { ThemeSelector } from '../../_providers/Theme/ThemeSelector'
-import { Gutter } from '../Gutter'
-import { CMSLink } from '../Link'
+import { fetchFooter } from '../../_api/fetchGlobals'
 
 import classes from './index.module.scss'
 
@@ -15,10 +12,7 @@ export async function Footer() {
   try {
     footer = await fetchFooter()
   } catch (error) {
-    // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // So swallow the error here and simply render the footer without nav items if one occurs
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+    
   }
 
   const navItems = footer?.navItems || []
